ec2d169ccab74e878a95f6f95615146971621dc2
Tai Kedzierski <dch.tai@gmail.com>
2018-02-06 15:07:55 +0000

    Add placeholder images, fix standalone comicarchive
    * Standalone comicarchive now runs properly and installs
    * A none-image asset is added to a libs folder on install
    * When a page's extension is undetected, none-image is used instead
    * Remove versioning artifact files from bbuild 4.x
    * Clean up directory when no pages downloaded

--- install.sh
+++ install.sh
@@ -2,11 +2,17 @@
 
 thisdir="$(dirname "$0")"
 bindir="$HOME/.local/bin"
+libdir="$HOME/.local/lib"
 
 if [[ "$UID" = 0 ]]; then
 	bindir=/usr/local/bin
+	libdir=/usr/local/lib
 else
 	mkdir -p "$bindir"
+	mkdir -p "$libdir"
 fi
 
-cp "$thisdir/bin/mfcom" "$bindir"
+cp "$thisdir/bin/mfcom" "$thisdir/bin/comicarchive" "$bindir"
+cp -r "$thisdir/assets" "$libdir/mfcom"
+
+echo "Done."
